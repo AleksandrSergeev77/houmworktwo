5f40f6d91e5ed08ddd2bae1f0e355523e8f1663e
AleksandrSergeev77 <52039460+AleksandrSergeev77@users.noreply.github.com>
2020-02-15 19:49:40 +0300

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,15 @@
+# описание транспорта
+name = 'автобус'
+# кол-во пассажиров
+passenger = 34
+# вид транспорта (личный или общественный)
+is_pabl_transport = True
+# вид топлива (бензин или дизель)
+is_gas_engine = True
+# максимальный вес
+max_weigth = 5.46
+print(type(name))
+print(type(passenger))
+print(type(is_pabl_transport))
+print(type(is_gas_engine))
+print(type(max_weigth))
